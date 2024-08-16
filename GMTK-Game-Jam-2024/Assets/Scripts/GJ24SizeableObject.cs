c63517ce5f6871c5d3a2735e22ab20c278c876cb
samuelmcdouall <samuelmcdouall@gmail.com>
2024-08-16 22:17:41 +0100

    Objects now move along the bottom + game over if hit object

--- GMTK-Game-Jam-2024/Assets/Scripts/GJ24SizeableObject.cs
+++ GMTK-Game-Jam-2024/Assets/Scripts/GJ24SizeableObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,9 @@ public class GJ24SizeableObject : MonoBehaviour
     [SerializeField]
     Transform _bottomPoint;
     float _groundLevel;
+    [SerializeField]
+    float _speed;
+    float _edgeOfViewableScreen = -200.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +20,14 @@ public class GJ24SizeableObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position += Vector3.left * _speed * Time.deltaTime;
         if (Mathf.Abs(_bottomPoint.transform.position.y - _groundLevel) > 0.1f)
         {
             transform.position += new Vector3(0.0f, _groundLevel - _bottomPoint.transform.position.y, 0.0f);
+        }
+        if (transform.position.x < _edgeOfViewableScreen)
+        {
+            Destroy(gameObject);
         }
     }
 
@@ -29,5 +38,14 @@ public class GJ24SizeableObject : MonoBehaviour
     public void ShrinkObject()
     {
         transform.localScale /= 2.0f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("GAME OVER");
+            Time.timeScale = 0.0f;
+        }
     }
 }
