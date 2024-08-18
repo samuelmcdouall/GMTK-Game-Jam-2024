fcf4dbc2f4539e2f91d58a46e6b5ae6ece2a6ce1
samuelmcdouall <samuelmcdouall@gmail.com>
2024-08-18 12:46:11 +0100

    Update GJ24Rotate.cs

--- GMTK-Game-Jam-2024/Assets/Scripts/GJ24Rotate.cs
+++ GMTK-Game-Jam-2024/Assets/Scripts/GJ24Rotate.cs
@@ -7,9 +7,41 @@ public class GJ24Rotate : MonoBehaviour
     [SerializeField]
     [Range(0.1f, 60.0f)]
     float revolution_time;
+    [SerializeField]
+    Direction _setDirection;
+    Vector3 _directionVector;
+    [SerializeField]
+    bool _invertDirection;
+
+    void Start()
+    {
+        if (_setDirection == Direction.X)
+        {
+            _directionVector = Vector3.right;
+        }
+        else if (_setDirection == Direction.Y)
+        {
+            _directionVector = Vector3.up;
+        }
+        else if (_setDirection == Direction.Z)
+        {
+            _directionVector = Vector3.forward;
+        }
+        if (_invertDirection)
+        {
+            _directionVector = -_directionVector;
+        }
+    }
 
     void Update()
     {
-        transform.Rotate(Vector3.forward, Time.deltaTime * 360.0f / revolution_time);
+        transform.Rotate(_directionVector, Time.deltaTime * 360.0f / revolution_time);
+    }
+
+    enum Direction
+    {
+        X,
+        Y,
+        Z
     }
 }
